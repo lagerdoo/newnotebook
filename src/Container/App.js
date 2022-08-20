9495b9b86d2b84d6fbcea5db68c57a8efdfbd76a
lagerdoo <eng.karimi.mohsen@gmail.com>
2022-08-21 01:38:00 +0200

    create login-page

---
+++ src/Container/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Login from '../Components/LoginForm';
+
+function App() {
+  return (
+    <div className="container-fluid App ">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
